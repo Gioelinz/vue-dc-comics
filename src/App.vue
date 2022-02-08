c7b5555ff9ef0082d7815f82461301fce6c9f302
Gioelinz <gioelerunza.gr@gmail.com>
2022-02-08 15:53:23 +0100

    add first header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style lang="scss">
+/* Generics */
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Rubik", sans-serif;
+  font-weight: 500;
+}
+
+.container {
+  max-width: 1400px;
+  margin: 0 auto;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="nav-container">
+        <figure>
+          <img src="../assets/img/dc-logo.png" alt="DC Logo" />
+        </figure>
+        <nav>
+          <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#" class="active">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/vars.scss";
+
+.nav-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 15px 0;
+}
+
+ul {
+  display: flex;
+  padding: 2rem 0;
+}
+
+ul li {
+  list-style-type: none;
+  padding: 1rem;
+}
+ul li a {
+  text-decoration: none;
+  color: black;
+  font-size: 0.9rem;
+}
+
+ul li a.active {
+  color: $primary_color;
+}
+</style>
+
